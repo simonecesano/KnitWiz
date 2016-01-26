9f79dc2a84aa83a7b8f762995e158620bd15f87c
Simone Cesano <simonecesano@libero.it>
2016-01-26 06:22:41 +0100

    Starting rasterization params

--- knitwiz.pl
+++ knitwiz.pl
@@ -43,15 +43,4 @@ get '/r/:md5/finish' => sub {
     my $c = shift;
     $c->render(text => '/r/:project/finish');
 };
-
-__DATA__
-
 app->start;
-__DATA__
-
-@@ index.html.ep
-% layout 'default';
-% title 'Welcome';
-<h1>Welcome to the Mojolicious real-time web framework!</h1>
-To learn more, you can browse through the documentation
-<%= link_to 'here' => '/perldoc' %>.
